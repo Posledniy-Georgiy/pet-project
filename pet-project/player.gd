56aaa211d4845387a74a8f995b77e429b456095b
Posledniy-Georgiy <carharttovich@gmail.com>
2026-02-03 02:39:23 +0300

    continue to slide
    добавил анимки

--- pet-project/player.gd
+++ pet-project/player.gd
@@ -1,48 +1,98 @@
 extends CharacterBody2D
 
+enum{
+	MOVE,
+	ATTACK,
+	ATTACK2,
+	ATTACK3,
+	BLOCK,
+	SLIDE
+}
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
+@onready var animPlayer = $AnimationPlayer
 var health = 100
 var gold = 0
+var state = MOVE
+var run_speed = 1
 
 func _physics_process(delta: float) -> void:
+	match state:
+		MOVE:
+			move_state()
+		ATTACK:
+			pass
+		ATTACK2:
+			pass
+		ATTACK3:
+			pass
+		BLOCK:
+			block_state()
+		SLIDE:
+			slide_state()
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			anim.play("run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
-			anim.play("idle")
-	
-	if direction == -1:
-		$AnimatedSprite2D.flip_h = true
-		
-	elif direction == 1:
-		$AnimatedSprite2D.flip_h = false
-		
 	if velocity.y > 0:
-		anim.play("fall")
+		animPlayer.play("fall")
+
+
 	if health <= 0:
+		health = 0
+		animPlayer.play("death")
+		await animPlayer.animation_finished
 		queue_free()
 		get_tree().change_scene_to_file("res://menu.tscn")
 
 
 	move_and_slide()
+
+
+func move_state():
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED * run_speed
+		if velocity.y == 0:
+			if run_speed == 1:
+				animPlayer.play("walk")
+			else:
+				animPlayer.play("run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y == 0:
+			animPlayer.play("idle")
+	if direction == -1:
+		anim.flip_h = true
+		
+	elif direction == 1:
+		anim.flip_h = false
+	if Input.is_action_pressed("run"):
+		run_speed = 2
+	else:
+		run_speed = 1
+	if Input.is_action_pressed("block"):
+		if velocity.x == 0:
+			state = BLOCK
+		else:
+			state = SLIDE
+
+
+func block_state():
+	velocity.x = 0
+	animPlayer.play("block")
+	if Input.is_action_just_released("block"):
+		state = MOVE
+	
+	
+func slide_state():
+	animPlayer.play("slide")
+	await animPlayer.animation_finished
+	state = MOVE
