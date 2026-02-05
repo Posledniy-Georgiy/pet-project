7b4b95f14e8cfdd103a80a9eb490ebb226f8a084
Posledniy-Georgiy <carharttovich@gmail.com>
2026-02-06 00:11:31 +0300

    state machine закончил

--- pet-project/player.gd
+++ pet-project/player.gd
@@ -20,17 +20,20 @@ var health = 100
 var gold = 0
 var state = MOVE
 var run_speed = 1
+var combo = false
+var attack_cooldown = false
+
 
 func _physics_process(delta: float) -> void:
 	match state:
 		MOVE:
 			move_state()
 		ATTACK:
-			pass
+			attack_state()
 		ATTACK2:
-			pass
+			attack2_state()
 		ATTACK3:
-			pass
+			attack3_state()
 		BLOCK:
 			block_state()
 		SLIDE:
@@ -84,6 +87,9 @@ func move_state():
 		else:
 			state = SLIDE
 
+	if Input.is_action_just_pressed("attack") and attack_cooldown == false:
+		state = ATTACK
+
 
 func block_state():
 	velocity.x = 0
@@ -96,3 +102,39 @@ func slide_state():
 	animPlayer.play("slide")
 	await animPlayer.animation_finished
 	state = MOVE
+
+
+func attack_state():
+	if Input.is_action_just_pressed("attack") and combo == true:
+		state = ATTACK2
+	velocity.x = 0
+	animPlayer.play("attack")
+	await animPlayer.animation_finished
+	attack_freeze()
+	state = MOVE
+
+func attack2_state():
+	if Input.is_action_just_pressed("attack") and combo == true:
+		state = ATTACK3
+	animPlayer.play("attack2")
+	await animPlayer.animation_finished
+	state = MOVE
+
+
+func attack3_state():
+	animPlayer.play("attack3")
+	await animPlayer.animation_finished
+	state = MOVE
+
+
+func combo1():
+	combo = true
+	await animPlayer.animation_finished
+	combo = false
+
+
+func attack_freeze():
+	attack_cooldown = true
+	await get_tree().create_timer(0.5).timeout
+	attack_cooldown = false
+	
